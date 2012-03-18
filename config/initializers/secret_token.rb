21300194c63217d7241d2fbd620770659435b41e
sakudevel <sakudevel@gmail.com>
2012-03-19 00:17:56 +0900

    create new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyDeAdmin::Application.config.secret_token = 'a9492d2aea3df800dcddaa0d2e29e8375cdfb90758cfefabb94d99e87d66d621e738bb76d0a95308cae52d097844e339d79c3e1cd80e815b12106c4b0cb565bb'
